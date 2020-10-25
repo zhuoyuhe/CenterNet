fcff447275de7b46ccf8dfe827d7416a15fff2a8
zhuoyuhe <zhuoyuhe@seas.upenn.edu>
2020-10-25 01:11:32 -0400

    debug

--- src/tools/kitti_eval/evaluate_object_3d_offline.cpp
+++ src/tools/kitti_eval/evaluate_object_3d_offline.cpp
@@ -830,7 +830,7 @@ bool eval(string gt_dir, string result_dir, Mail* mail){
     // read ground truth and result poses
     bool gt_success,det_success;
     vector<tGroundtruth> gt   = loadGroundtruth(gt_dir + "/" + file_name,gt_success);
-    vector<tDetection>   det  = loadDetections(result_dir + file_name,
+    vector<tDetection>   det  = loadDetections(result_dir + "/"+ file_name,
             compute_aos, eval_image, eval_ground, eval_3d, det_success);
     groundtruth.push_back(gt);
     detections.push_back(det);
